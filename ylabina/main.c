1018b40a2846b18f27b5e7f00a0ec8eb71f1b3c9
Anwar Abd El Hamid <anwarelrabie@gmail.com>
2016-02-02 21:56:21 +0200

    7fns

--- ylabina/main.c
+++ ylabina/main.c
@@ -15,64 +15,65 @@ void blink3();
 void blink4();
 void blink5();
 void blink6();
+void blink7();
 
 int main() {
 	while (1) {
-		//DIO_u8WritePortDir(DIO_u8PORT1, DIO_u8PORTOUTPUT);
-		//DIO_u8WritePortVal(DIO_u8PORT1, 0xFF);
+		DIO_u8WritePortDir(DIO_u8PORT2, DIO_u8PORTOUTPUT);
+		DIO_u8WritePortVal(DIO_u8PORT2, 0xFF);
 
-//blink2();
-//blink1();
-//blink3();
-//blink4();
-//blink5();
+blink2();
+blink1();
+blink3();
+blink4();
+blink5();
 blink6();
-
+blink7();
 	}
 	return 0;
 }
 
 void blink1(){
-	DIO_u8WritePortDir(DIO_u8PORT1, DIO_u8PORTOUTPUT);
-	DIO_u8WritePortVal(DIO_u8PORT1, 0xAA);
+	DIO_u8WritePortDir(DIO_u8PORT2, DIO_u8PORTOUTPUT);
+	DIO_u8WritePortVal(DIO_u8PORT2, 0xAA);
 	Delay(70);
-	DIO_u8WritePortVal(DIO_u8PORT1, 0x55);
+	DIO_u8WritePortVal(DIO_u8PORT2, 0x55);
 	Delay(70);
 }
 
 void blink2(){
-	DIO_u8WritePortDir(DIO_u8PORT1, DIO_u8PORTOUTPUT);
-	DIO_u8WritePortVal(DIO_u8PORT1, 0x00);
+	DIO_u8WritePortDir(DIO_u8PORT2, DIO_u8PORTOUTPUT);
+	DIO_u8WritePortVal(DIO_u8PORT2, 0x00);
 	Delay(70);
-	DIO_u8WritePortVal(DIO_u8PORT1, 0xFF);
+	DIO_u8WritePortVal(DIO_u8PORT2, 0xFF);
 	Delay(70);
 }
 
 
 
 void blink3(){
-	DIO_u8WritePortDir(DIO_u8PORT1, DIO_u8PORTOUTPUT);
-	DIO_u8WritePortVal(DIO_u8PORT1, 0x0F);
+	DIO_u8WritePortDir(DIO_u8PORT2, DIO_u8PORTOUTPUT);
+	DIO_u8WritePortVal(DIO_u8PORT2, 0x0F);
 	Delay(70);
-	DIO_u8WritePortVal(DIO_u8PORT1, 0xF0);
+	DIO_u8WritePortVal(DIO_u8PORT2, 0xF0);
 	Delay(70);
 }
 
 void blink4(){
 u8 x=1;
 u8 y=128;
-DIO_u8WritePortDir(DIO_u8PORT1, DIO_u8PORTOUTPUT);
+DIO_u8WritePortDir(DIO_u8PORT2, DIO_u8PORTOUTPUT);
 
 for(u8 i=0;i<8;i++)
 {
-	DIO_u8WritePortVal(DIO_u8PORT1, x);
+	DIO_u8WritePortVal(DIO_u8PORT2, x);
 	x=x<<1;
 	Delay(20);
 }
 
 for(u8 i=0;i<8;i++)
 {
-DIO_u8WritePortVal(DIO_u8PORT1, y);
+DIO_u8WritePortVal(DIO_u8PORT2, y);
 	Delay(20);
 	y=y>>1;
 }
@@ -81,12 +82,12 @@ DIO_u8WritePortVal(DIO_u8PORT1, y);
 void blink5(){
 u8 old_x=0;
 
-DIO_u8WritePortDir(DIO_u8PORT1, DIO_u8PORTOUTPUT);
+DIO_u8WritePortDir(DIO_u8PORT2, DIO_u8PORTOUTPUT);
 
 for(u8 i=0;i<8;i++)
 {
 	old_x=old_x*2+1;
-	DIO_u8WritePortVal(DIO_u8PORT1, old_x);
+	DIO_u8WritePortVal(DIO_u8PORT2, old_x);
 	Delay(20);
 }
 }
@@ -96,53 +97,34 @@ void blink6(){
 u8 old_x=0;
 u8 old_x2=128;
 
-DIO_u8WritePortDir(DIO_u8PORT1, DIO_u8PORTOUTPUT);
+DIO_u8WritePortDir(DIO_u8PORT2, DIO_u8PORTOUTPUT);
 
 for(u8 i=0;i<4;i++)
 {
 	old_x=old_x*2+1;
-	DIO_u8WritePortVal(DIO_u8PORT1, old_x2|old_x);
+	DIO_u8WritePortVal(DIO_u8PORT2, old_x2|old_x);
 	old_x2|=(old_x2/2);
 	Delay(20);
 }
 }
 
 
+void blink7(){
+u8 x=3;
+DIO_u8WritePortDir(DIO_u8PORT2, DIO_u8PORTOUTPUT);
 
+for(u8 i=0;i<3;i++)
+{
+	DIO_u8WritePortVal(DIO_u8PORT2, x);
+	x*=4;
+	Delay(20);
+}
 
-//DIO_voidInit();
-/*
-DIO_u8WritePortDir(DIO_u8PORT1, DIO_u8PORTOUTPUT);
+for(u8 i=0;i<4;i++)
+{
+	DIO_u8WritePortVal(DIO_u8PORT2, x);
+	x/=4;
+	Delay(20);
+}
+}
 
-DIO_u8WritePortVal(DIO_u8PORT1, 0xAA);
-Delay(100);
-
-DIO_u8WritePortVal(DIO_u8PORT1, 0x55);
-Delay(100);
-*/
-/*
-DIO_u8WritePinDir(8,1);
- DIO_u8WritePinDir(10,1);
-
- DIO_u8WritePinVal(8,1);
- DIO_u8WritePinVal(10,0);
- Delay(100);
- DIO_u8WritePinVal(8,0);
-		 DIO_u8WritePinVal(10,1);
-		 Delay(100);
-*/
-
-
-//DIO_u8WritePinDir(16,0);
-//DIO_u8WritePinDir(17,0);
-/*
-DIO_u8WritePinDir(8,1);
-DIO_u8WritePinDir(10,1);
-DIO_u8WritePortDir(2,0xFF);
-DIO_u8WritePortVal(DIO_u8PORT2, 0xFF);
-  u8 check_1=0;
-  u8 check_2=0;
-  DIO_u8ReadPinVal(16,&check_1);
-  DIO_u8ReadPinVal(17,&check_2);
-  DIO_u8WritePinVal(10,check_1);
-  */
